33aa05b72dda1450330105cb39d71decf75ffc1f
Yunkeun <yunx2@naver.com>
2022-02-07 15:30:22 +0900

    Initial Commit

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,8 @@
 package racingcar;
 
-import racingcar.controller.GameController;
-
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO 구현 진행
-		// 수정한 부분: MVC 패턴에 의거 input, output은 Controller에서 하는게 알맞다.
-		GameController controller = new GameController();
-		controller.playRacing();
 	}
 }
