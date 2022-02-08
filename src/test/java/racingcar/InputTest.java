11d2a0101d292ace50c119aca1019b35bce9cc3c
Yunkeun <yunx2@naver.com>
2022-02-08 17:38:29 +0900

    refactor: 자동차 이름 쉼표 구분 테스트 리팩토링

--- src/test/java/racingcar/InputTest.java
+++ src/test/java/racingcar/InputTest.java
@@ -4,23 +4,22 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import racingcar.view.InputView;
 
 public class InputTest {
-
-	private static final String COMMA = ",";
 
 	@Test
 	void 자동차_이름_쉼표_구분() {
 		//given
-		String answer1 = "car1";
-		String answer2 = "car2";
+		final String answer1 = "car1";
+		final String answer2 = "car2";
 		final InputView inputView = new InputView();
 
 		//when
-		String carsName1 = "car1";
-		String carsName2 = "car1,car2";
-		final List<String> carsNameList1 = InputView.divideNameBy(carsName1, COMMA);
-		final List<String> carsNameList2 = InputView.divideNameBy(carsName2, COMMA);
+		final String carsName1 = "car1";
+		final String carsName2 = "car1,car2";
+		final List<String> carsNameList1 = inputView.splitName(carsName1);
+		final List<String> carsNameList2 = inputView.splitName(carsName2);
 
 		//then
 		assertThat(carsNameList1.contains(answer1)).isEqualTo(true);
