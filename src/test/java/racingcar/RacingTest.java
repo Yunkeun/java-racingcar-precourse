2c385290df5c4bd5352fc54922a7e1c84169fbdd
Yunkeun <yunx2@naver.com>
2022-02-11 18:26:03 +0900

    fix: 전진하는 조건 테스트 수정

--- src/test/java/racingcar/RacingTest.java
+++ src/test/java/racingcar/RacingTest.java
@@ -1,13 +1,11 @@
 package racingcar;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import racingcar.car.model.Car;
 
 public class RacingTest {
-
-	private static final int MINIMUM_NUMBER_TO_MOVE = 4;
-	private static final int MOVE_FORWARD_ONE = 1;
 
 	@Test
 	void 전진_조건_테스트() {
@@ -16,13 +14,9 @@ public class RacingTest {
 		final Car car = new Car(carName);
 		final int currentPosition = car.getPosition();
 		//when
-		final int resultNumber = car.checkMoveForward();
-		car.moveForward(resultNumber);
+		car.moveForward();
 		//then
-		if (resultNumber >= MINIMUM_NUMBER_TO_MOVE) {
-			Assertions.assertThat(currentPosition + MOVE_FORWARD_ONE).isEqualTo(car.getPosition());
-			return;
-		}
-		Assertions.assertThat(currentPosition).isEqualTo(car.getPosition());
+		boolean isSuccessTest = car.getPosition() - currentPosition == 0 || car.getPosition() - currentPosition == 1;
+		assertThat(isSuccessTest).isEqualTo(true);
 	}
 }
