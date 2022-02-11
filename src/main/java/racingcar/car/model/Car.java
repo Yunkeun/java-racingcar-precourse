6f7db20160ce3059276060aeb310a5401b1f7875
Yunkeun <yunx2@naver.com>
2022-02-11 18:27:28 +0900

    feat: 전진하는 조건에 따른 자동차 전진 기능 구현

--- src/main/java/racingcar/car/model/Car.java
+++ src/main/java/racingcar/car/model/Car.java
@@ -2,11 +2,16 @@ package racingcar.car.model;
 
 import static racingcar.util.ErrorMessage.*;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 
 	private final String name;
 	private int position = 0;
 	private static final int MAX_LENGTH = 5;
+	private static final int MINIMUM_RANDOM_NUMBER = 0;
+	private static final int MAXIMUM_RANDOM_NUMBER = 9;
+	private static final int MINIMUM_NUMBER_TO_MOVE = 4;
 
 	public Car(String name) {
 		validateNameLength(name);
@@ -17,5 +22,20 @@ public class Car {
 		if (name.isEmpty() || name.length() > MAX_LENGTH) {
 			throw new IllegalArgumentException(ERROR_HEADER + LENGTH_ERROR_MESSAGE);
 		}
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void moveForward() {
+		if (checkMoveForward()) {
+			position++;
+		}
+	}
+
+	public boolean checkMoveForward() {
+		final int resultNumber = Randoms.pickNumberInRange(MINIMUM_RANDOM_NUMBER, MAXIMUM_RANDOM_NUMBER);
+		return resultNumber >= MINIMUM_NUMBER_TO_MOVE;
 	}
 }
