183fa37f2f583f8246bd5dd57f1014dc006b483a
Yunkeun <yunx2@naver.com>
2022-02-12 16:22:48 +0900

    test: 전진 조건 테스트를 하나의 자동차 레이스 테스트로 변경 car 객체 내에서 진행하기 보다는 service 로직을 분리하여 service 객체에서 진행하기를 원한다.

--- src/test/java/racingcar/RacingTest.java
+++ src/test/java/racingcar/RacingTest.java
@@ -2,21 +2,29 @@ package racingcar;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Collections;
 import org.junit.jupiter.api.Test;
 import racingcar.car.model.Car;
+import racingcar.car.model.Cars;
+import racingcar.car.service.CarService;
+import racingcar.car.service.CarServiceImpl;
 
 public class RacingTest {
 
+	private final CarService carService = new CarServiceImpl();
+
 	@Test
-	void 전진_조건_테스트() {
+	void 하나의_자동차_레이스_테스트() {
 		//given
 		final String carName = "car1";
 		final Car car = new Car(carName);
 		final int currentPosition = car.getPosition();
+		final Cars cars = new Cars(Collections.singletonList(new Car(carName)));
 		//when
-		car.moveForward();
+		carService.race(cars);
 		//then
-		boolean isSuccessTest = car.getPosition() - currentPosition == 0 || car.getPosition() - currentPosition == 1;
+		boolean isSuccessTest = car.getPosition() - currentPosition == 0
+			|| car.getPosition() - currentPosition == 1;
 		assertThat(isSuccessTest).isEqualTo(true);
 	}
 }
